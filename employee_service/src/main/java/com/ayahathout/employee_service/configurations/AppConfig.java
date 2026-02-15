a843929c44911a51ac23cdff42b2caa4f5ea72e5
Aya <ayahathout1@gmail.com>
2026-02-15 14:09:55 +0200

    enhace some code in employee-service

--- employee_service/src/main/java/com/ayahathout/employee_service/configurations/AppConfig.java
+++ employee_service/src/main/java/com/ayahathout/employee_service/configurations/AppConfig.java
@@ -1,6 +1,7 @@
 package com.ayahathout.employee_service.configurations;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ public class AppConfig {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
