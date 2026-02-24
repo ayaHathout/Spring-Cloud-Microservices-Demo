633f09f88ccf466a72c942698c76daf23a8fe65c
Aya <ayahathout1@gmail.com>
2026-02-24 14:32:59 +0200

    add api gateway with routes and load balancing

---
+++ api-gateway-with-rate-limiter/src/main/java/com/ayahathout/api_gateway_with_rate_limiter/ApiGatewayWithRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.ayahathout.api_gateway_with_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayWithRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayWithRateLimiterApplication.class, args);
+	}
+
+}
